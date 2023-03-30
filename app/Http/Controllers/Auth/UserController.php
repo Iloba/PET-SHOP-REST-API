b18cdcb5f82d8a6784af17aaf1762815b2361e96
Iloba <ilobatimothy@gmail.com>
2023-03-31 00:44:01 +0100

    chore: installed swagger ui

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -17,6 +17,29 @@ use App\Models\AccessToken;
 
 class UserController extends APIController
 {
+
+     /**
+     * @OA\Get(
+     *      path="/projects",
+     *      operationId="getProjectsList",
+     *      tags={"Projects"},
+     *      summary="Get list of projects",
+     *      description="Returns list of projects",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/ProjectResource")
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
     public function store(CreateUserRequest $request, CreateUserService $createUserService): JsonResponse
     {
         $user = $createUserService->create($request->validated());
--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -19,7 +19,7 @@ class AdminSeeder extends Seeder
             'first_name' => 'Admin',
             'last_name' => 'User',
             'is_admin' => true,
-            'email' => 'adminuser@gmail.com',
+            'email' => 'adminuser@example.com',
             'password' => Hash::make('password'),
             'address' => "No 7 Admin Close Malali Kaduna",
             'phone_number' => "091098771985",
